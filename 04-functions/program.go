31b3165bbdf396ecf6bc4bf2014abdacdd89f462
Magesh Kuppan <tkmagesh77@gmail.com>
2021-05-24 15:19:13 +0530

    functions - part - 1

---
+++ 04-functions/program.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+var add func(int, int) int = func(x, y int) int {
+	return x + y
+}
+
+func main() {
+	x, y := 100, 200
+	/*
+		add := func(x, y int) int {
+			return x + y
+		}
+	*/
+	fmt.Printf("Adding %d and %d results in %d \n", x, y, add(x, y))
+	quotient, remainder := divide(70, 8)
+	fmt.Println(quotient, remainder)
+}
+
+/*
+func add(x, y int) int {
+	return x + y
+}
+*/
+
+/*
+func divide(x, y int) (int, int) {
+	//fmt.Println(float32(x) / float32(y))
+	quotient := x / y
+	remainder := x % y
+	return quotient, remainder
+}
+*/
+
+func divide(x, y int) (quotient int, remainder int) {
+	//fmt.Println(float32(x) / float32(y))
+	quotient = x / y
+	remainder = x % y
+	return
+}
