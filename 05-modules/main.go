3da00fae9e82eff6ded671f60c814bf1035316f7
Magesh Kuppan <tkmagesh77@gmail.com>
2021-05-25 11:55:18 +0530

    using third party packages

--- 05-modules/main.go
+++ 05-modules/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	cal "modularity-demo/calculator"
+
+	"github.com/fatih/color"
 )
 
 func main() {
 	fmt.Println(cal.Add(100, 200))
 	fmt.Println(cal.Subtract(100, 200))
 	fmt.Println(cal.GetCalcCount())
-	fmt.Println(greet("Magesh"))
+	color.Blue(greet("Magesh"))
 }
