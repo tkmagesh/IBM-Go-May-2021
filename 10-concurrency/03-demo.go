2b82b93270b4e3c001510241b3ce2022682350ed
Magesh Kuppan <tkmagesh77@gmail.com>
2021-05-26 15:17:05 +0530

    using channels

--- 10-concurrency/03-demo.go
+++ 10-concurrency/03-demo.go
@@ -1,13 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-func add(x, y int) int {
-	result := x + y
-	return result
+/*
+	The following is an example for "Sharing memory for communication" - not preferred
+	The preferred solution is "Communicate by sharing memory" (04-demo.go)
+*/
+var result int
+
+var wg *sync.WaitGroup = &sync.WaitGroup{}
+
+func add(x, y int) {
+	result = x + y
+	wg.Done()
 }
 
 func main() {
-	result := add(100, 200)
+	wg.Add(1)
+	go add(100, 200)
+	wg.Wait()
 	fmt.Printf("Result = %v\n", result)
 }
---
+++ 10-concurrency/04-demo.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func add(x, y int, ch chan int) {
+	result := x + y
+	//writing data into the channel
+	fmt.Println("Before writing data into the channel")
+	ch <- result
+	fmt.Println("After writing data into the channel")
+}
+
+func main() {
+	resultCh := make(chan int)
+	go add(100, 200, resultCh)
+	<-resultCh
+	/*fmt.Println("Result = ", result) */
+	var input string
+	fmt.Scanln(&input)
+}
